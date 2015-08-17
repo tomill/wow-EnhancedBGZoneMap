2bd4d29ac00d4f5ff7aa7f04bce090f6d4eec762
Naoki Tomita <n.tomita@gmail.com>
2015-08-18 00:12:26 +0900

    added option panel

--- core.lua
+++ core.lua
@@ -1,161 +1,170 @@
+-- required
 LoadAddOn("Blizzard_BattlefieldMinimap")
 
--- coordes for Interface/Minimap/PartyRaidBlips
-local BLIP_TEX_COORDS = { 
-    ["WARRIOR"]     = { 0, 0.125, 0, 0.25 }, 
-    ["PALADIN"]     = { 0.125, 0.25, 0, 0.25 }, 
-    ["HUNTER"]      = { 0.25, 0.375, 0, 0.25 }, 
-    ["ROGUE"]       = { 0.375, 0.5, 0, 0.25 }, 
-    ["PRIEST"]      = { 0.5, 0.625, 0, 0.25 }, 
-    ["DEATHKNIGHT"] = { 0.625, 0.75, 0, 0.25 }, 
-    ["SHAMAN"]      = { 0.75, 0.875, 0, 0.25 }, 
-    ["MAGE"]        = { 0.875, 1, 0, 0.25 }, 
-    ["WARLOCK"]     = { 0, 0.125, 0.25, 0.5 }, 
-    ["DRUID"]       = { 0.25, 0.375, 0.25, 0.5 }, 
-    ["MONK"]        = { 0.125, 0.25, 0.25, 0.5 }, 
+-- coordes for Interface/Minimap/PartyRaidBlips.blp
+local BLIP_TEX_COORDS = {
+    ["WARRIOR"]     = { 0,     0.125, 0,    0.25 },
+    ["PALADIN"]     = { 0.125, 0.25,  0,    0.25 },
+    ["HUNTER"]      = { 0.25,  0.375, 0,    0.25 },
+    ["ROGUE"]       = { 0.375, 0.5,   0,    0.25 },
+    ["PRIEST"]      = { 0.5,   0.625, 0,    0.25 },
+    ["DEATHKNIGHT"] = { 0.625, 0.75,  0,    0.25 },
+    ["SHAMAN"]      = { 0.75,  0.875, 0,    0.25 },
+    ["MAGE"]        = { 0.875, 1,     0,    0.25 },
+    ["WARLOCK"]     = { 0,     0.125, 0.25, 0.5  },
+    ["DRUID"]       = { 0.25,  0.375, 0.25, 0.5  },
+    ["MONK"]        = { 0.125, 0.25,  0.25, 0.5  },
 }
 
-local mapScale = 1.8
-local pointScale = 1.2
-local arrowScale = 1.3
-local nameScale = 0.6
-local nameLength = 2
-
-local classCount = { }
-
+local addon = LibStub("AceAddon-3.0"):NewAddon("EnhancedBGZoneMap")
+local tick = CreateFrame("Frame")
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("GROUP_ROSTER_UPDATE")
 frame:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
 frame:RegisterEvent("PLAYER_ENTERING_BATTLEGROUND")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("PLAYER_LEAVING_WORLD")
+frame:RegisterEvent("UPDATE_WORLD_STATES")
+frame:RegisterEvent("WORLD_MAP_UPDATE")
 
-
-local function setupZonemap()
-    BattlefieldMinimapBackground:Hide()
-    BattlefieldMinimapCorner:Hide()
-    BattlefieldMinimapCloseButton:Hide()
-    BattlefieldMinimap:SetScale(mapScale)
-    BattlefieldMinimap:Show()
-end
-
-
-local function setMakerSize(marker)
-    if marker.scaleChanged then return end
+frame:SetScript("OnEvent", function(self, event)
+    if (event == "ADDON_LOADED") then
+        addon:setupZonemap()
+    end
     
-    local h = marker:GetHeight()
-    local w = marker:GetWidth()
-    if UnitIsUnit(marker.unit, "player") then
-        marker:SetHeight(h * arrowScale)
-        marker:SetWidth(w * arrowScale)
+    addon:updateMarkers()
+    
+    if UnitInBattleground("player") then
+        tick:Show()
     else
-        marker:SetHeight(h * pointScale)
-        marker:SetWidth(w * pointScale)
+        tick:Hide()
     end
+end)
+
+tick:SetScript("OnUpdate", function(self, elapsed)
+    addon.last_updated = (addon.last_updated or 0) + elapsed
+    if addon.last_updated > 1 then
+        addon.last_updated = 0
+        addon:updateMarkers()
+    end
+end)
+
+hooksecurefunc("BattlefieldMinimap_OnUpdate", function()
+    addon:updateMarkers()
+end)
     
-    marker:SetFrameStrata("HIGH")
-    marker.scaleChanged = true
+SLASH_UPDATEZONEMAPNAMELABEL1 = "/enhancedbgzonemap"
+SLASH_UPDATEZONEMAPNAMELABEL2 = "/zonemap"
+
+SlashCmdList["UPDATEZONEMAPNAMELABEL"] = function (opt)
+    addon:updateMarkers()
+    print("EnhancedBGZoneMap: updated")
 end
 
+function addon:OnInitialize()
+    local setter = function(info, val) addon.db.profile[ info[#info] ] = val; addon:setupZonemap() end
+    local getter = function(info) return addon.db.profile[ info[#info] ] end
+    
+    local defaults = {
+        profile = {
+            hideBorder = false,
+            mapScale = 1.5,
+            arrowScale = 1,
+            pointScale = 0.5,
+        }
+    }
 
-local function setMakerTexture(marker)
-    local _, class = UnitClass(marker.unit)
-    marker.icon:SetTexture("Interface\\Minimap\\PartyRaidBlips")
-    marker.icon:SetTexCoord(unpack(BLIP_TEX_COORDS[class]))
+    local options = {
+        type = "group",
+        args = {
+            mapScale = {
+                order = 11,
+                type = "range",
+                name = "Zonemap frame scale",
+                min = 0.5,
+                max = 5,
+                set = setter,
+                get = getter,
+            },
+            
+            arrowScale = {
+                order = 12,
+                type = "range",
+                name = "Allow marker scale",
+                min = 0.01,
+                max = 3,
+                set = setter,
+                get = getter,
+            },
+            pointScale = {
+                order = 13,
+                type = "range",
+                name = "Party player marker",
+                min = 0.01,
+                max = 3,
+                set = setter,
+                get = getter,
+            },
+            hideBorder = {
+                order = 15,
+                type = "toggle",
+                name = "Hide zonemap border",
+                width = "full",
+                set = setter,
+                get = getter,
+            },
+        }
+    }
+    
+    self.db = LibStub("AceDB-3.0"):New(self.name .. "DB", defaults)
+    LibStub("AceConfig-3.0"):RegisterOptionsTable(self.name, options)
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(self.name)
 end
 
-local function setNameLabel(marker)
-    if not marker.nametext then
-        marker.nametext = marker:CreateFontString(marker:GetName() .. "NameText", "ARTWORK", "GameFontHighlightSmall")
-        marker.nametext:SetFont("Fonts\\FRIZQT__.TTF", marker:GetHeight() * nameScale)
+function addon:setupZonemap()
+    if addon.db.profile.hideBorder then
+        BattlefieldMinimapBackground:Hide()
+        BattlefieldMinimapCorner:Hide()
+        BattlefieldMinimapCloseButton:Hide()
+    else
+        BattlefieldMinimapBackground:Show()
+        BattlefieldMinimapCorner:Show()
+        BattlefieldMinimapCloseButton:Show()
     end
     
-    marker.nametext:Hide()
+    BattlefieldMinimap:SetScale(addon.db.profile.mapScale)
+    BattlefieldMinimap:Show()
     
-    local _, class = UnitClass(marker.unit)
-    if (classCount[class] and classCount[class] > 1) then
-        local name = UnitName(marker.unit)
-        name = string.sub(name, 0, nameLength)
-        
-        local color = RAID_CLASS_COLORS[class]
-        marker.nametext:SetText(name)
-        marker.nametext:SetTextColor(color.r, color.g, color.b)
-        
-        if (tonumber(string.sub(marker.unit, -1)) % 2 == 0) then
-            marker.nametext:SetPoint("LEFT", marker, "RIGHT")
-        else
-            marker.nametext:SetPoint("RIGHT", marker, "LEFT")
-        end
-        
-        marker.nametext:Show()
-    end
+    addon:updateMarkers()
 end
 
--- local unit2pos = {}
--- local matrix2num = {}
--- local function getPosition(unit)
---     local x, y = GetPlayerMapPosition(unit)
---     x = string.format("%d", (x + 0.001) * 10) .. ""
---     y = string.format("%d", (y + 0.001) * 10) .. ""
---     
---     unit2pos[unit] = { x = x, y = y }
---     local matrix = x .. "-" .. y
---     matrix2num[matrix] = (matrix2num[matrix] or 0) + 1
--- end
-
-
-local function updateZonemap()
-    if not UnitInBattleground("player") then
-        return
-    end 
-    
+function addon:updateMarkers()
     local marker = _G["BattlefieldMinimapPlayer"]
     if marker then
-        setMakerSize(marker)
+        addon:setMaker(marker)
     end
     
     local memberCount = GetNumGroupMembers()
     for i = 1, memberCount do
         local marker = _G["BattlefieldMinimapRaid" .. i]
         if marker then
-            setMakerSize(marker)
-            setMakerTexture(marker)
-            setNameLabel(marker)
+            addon:setMaker(marker)
         end
     end
 end
 
-
-hooksecurefunc("BattlefieldMinimap_OnUpdate", function()
-    updateZonemap()
-end)
-    
-frame:SetScript("OnEvent", function(self, event)
-    if (event == "ADDON_LOADED") then
-        setupZonemap()
-        frame:UnregisterEvent("ADDON_LOADED")
-        return
+function addon:setMaker(marker)
+    local default = 24
+    if UnitIsUnit(marker.unit, "player") then
+        marker:SetHeight(default * addon.db.profile.arrowScale)
+        marker:SetWidth(default * addon.db.profile.arrowScale)
+    else
+        marker:SetHeight(default * addon.db.profile.pointScale)
+        marker:SetWidth(default * addon.db.profile.pointScale)
+        
+        local _, class = UnitClass(marker.unit)
+        marker.icon:SetTexture("Interface\\Minimap\\PartyRaidBlips")
+        marker.icon:SetTexCoord(unpack(BLIP_TEX_COORDS[class]))
     end
-    
-    classCount = { }
-    local memberCount = GetNumGroupMembers()
-    for i = 1, memberCount do
-        local _, class = UnitClass("raid" .. i)
-        classCount[class] = (classCount[class] or 0) + 1
-    end
-    
-    local _, class = UnitClass("player")
-    classCount[class] = (classCount[class] or 0) - 1
-    
-    if (event == "GROUP_ROSTER_UPDATE") then
-        updateZonemap()
-    end
-end)
-
-
-SLASH_UPDATEZONEMAPNAMELABEL1 = "/enhancedbgzonemap"
-SLASH_UPDATEZONEMAPNAMELABEL2 = "/zonemap"
-
-SlashCmdList["UPDATEZONEMAPNAMELABEL"] = function (opt)
-    updateZonemap()
-    print("EnhancedBGZoneMap updated")
 end
